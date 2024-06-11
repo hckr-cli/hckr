9aef9e56aec2f64bd0abc2bdad4297e7cc850025
Ashish Patel <ashishpatel0720@gmail.com>
2024-06-11 14:09:42 +0530

    setup: renaming

--- src/hckr/__main__.py
+++ src/hckr/__main__.py
@@ -4,6 +4,6 @@
 import sys
 
 if __name__ == "__main__":
-    from devd.cli import cli
+    from hckr.cli import cli
 
     sys.exit(cli())
--- src/hckr/cli/__init__.py
+++ src/hckr/cli/__init__.py
@@ -10,7 +10,7 @@ from ..__about__ import __version__
 #     context_settings={"help_option_names": ["-h", "--help"]},
 #     invoke_without_command=True,
 # )
-# @click.version_option(version=__version__, prog_name="devd")
+# @click.version_option(version=__version__, prog_name="hckr")
 # def cli():
 #     pass
 
@@ -19,12 +19,12 @@ from ..__about__ import __version__
     context_settings={"help_option_names": ["-h", "--help"]},
     invoke_without_command=True,
 )
-@click.version_option(version=__version__, prog_name="Devd")
+@click.version_option(version=__version__, prog_name="hckr")
 @click.pass_context
 def cli(ctx: click.Context):
-    click.secho("Devd ", fg="magenta", bold=True, nl=False)
+    click.secho("hckr ", fg="magenta", bold=True, nl=False)
     click.secho(f"v{__version__}  ", fg="blue", bold=True, nl=False)
-    click.secho(f"https://github.com/pateash/devd", fg="green", bold=True, nl=True)
+    click.secho(f"https://github.com/pateash/hckr", fg="green", bold=True, nl=True)
 
 
 @click.command()
@@ -35,12 +35,12 @@ def hello(name):
 
 @click.command()
 def version():
-    click.echo(f"devd v{__version__}")
+    click.echo(f"hckr v{__version__}")
 
 
 cli.add_command(hello)
 cli.add_command(version)
 
-# implementing this so that if user just uses `devd` we show them something
-if __name__ == "devd":
+# implementing this so that if user just uses `hckr` we show them something
+if __name__ == "hckr":
     cli()
