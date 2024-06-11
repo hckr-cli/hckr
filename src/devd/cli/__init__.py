c6ff158eaea4c1a564f69349e5a96da63c769cf7
Ashish Patel <ashishpatel0720@gmail.com>
2024-06-11 13:55:07 +0530

    setup: adding help text (#3)

--- src/devd/cli/__init__.py
+++ src/devd/cli/__init__.py
@@ -22,25 +22,27 @@ from ..__about__ import __version__
 @click.version_option(version=__version__, prog_name="Devd")
 @click.pass_context
 def cli(ctx: click.Context):
-    click.secho("Devd ", fg="magenta", bold=True, nl=False)
-    click.secho(f"v{__version__}  ", fg="blue", bold=True, nl=False)
-    click.secho(f"https://github.com/pateash/devd", fg="green", bold=True, nl=True)
-
+    if ctx.invoked_subcommand is None: # only devd is used then show help
+        click.secho("Devd ", fg="magenta", bold=True, nl=False)
+        click.secho(f"v{__version__}  ", fg="blue", bold=True, nl=True)
+        cli(["--help"])
 
 @click.command()
 @click.option("--name", help="Say hello to someone", required=False, default="World")
 def hello(name):
-    click.echo(f"Hello {name}!")
+    click.secho(f"Hello {name}!", fg="green", nl=True)
 
 
-@click.command()
-def version():
-    click.echo(f"devd v{__version__}")
-
+@click.command(help="Showing cli information information")
+def info():
+    click.secho("Version: devd", fg="magenta", bold=True, nl=False)
+    click.secho(f"=={__version__}  ", fg="blue", bold=False, nl=True)
+    click.secho(f"Github: https://github.com/pateash/devd", fg="red", bold=True, nl=True)
+    click.secho(f"PyPi: https://pypi.org/project/devd/", fg="yellow", bold=True, nl=True)
 
 cli.add_command(hello)
-cli.add_command(version)
+cli.add_command(info)
 
 # implementing this so that if user just uses `devd` we show them something
-if __name__ == "devd":
+if __name__ == "__main__":
     cli()
