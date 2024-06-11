3513bbbbb248e7d1a88f56b0f3bb29ae19e023ff
Ashish Patel <ashishpatel0720@gmail.com>
2024-06-11 14:22:03 +0530

    setup: renaming (#4)
    * setup: renaming
    
    * setup: renaming
    
    # Conflicts:
    #	src/hckr/cli/__init__.py
    
    * setup: renaming
    
    # Conflicts:
    #	src/hckr/cli/__init__.py

--- src/hckr/cli/__init__.py
+++ src/hckr/cli/__init__.py
@@ -10,7 +10,7 @@ from ..__about__ import __version__
 #     context_settings={"help_option_names": ["-h", "--help"]},
 #     invoke_without_command=True,
 # )
-# @click.version_option(version=__version__, prog_name="devd")
+# @click.version_option(version=__version__, prog_name="hckr")
 # def cli():
 #     pass
 
@@ -19,13 +19,13 @@ from ..__about__ import __version__
     context_settings={"help_option_names": ["-h", "--help"]},
     invoke_without_command=True,
 )
-@click.version_option(version=__version__, prog_name="Devd")
+@click.version_option(version=__version__, prog_name="hckr")
 @click.pass_context
 def cli(ctx: click.Context):
-    if ctx.invoked_subcommand is None: # only devd is used then show help
-        click.secho("Devd ", fg="magenta", bold=True, nl=False)
-        click.secho(f"v{__version__}  ", fg="blue", bold=True, nl=True)
-        cli(["--help"])
+    click.secho("hckr ", fg="magenta", bold=True, nl=False)
+    click.secho(f"v{__version__}  ", fg="blue", bold=True, nl=False)
+    click.secho(f"https://github.com/pateash/hckr", fg="green", bold=True, nl=True)
+
 
 @click.command()
 @click.option("--name", help="Say hello to someone", required=False, default="World")
@@ -37,8 +37,13 @@ def hello(name):
 def info():
     click.secho("Version: devd", fg="magenta", bold=True, nl=False)
     click.secho(f"=={__version__}  ", fg="blue", bold=False, nl=True)
-    click.secho(f"Github: https://github.com/pateash/devd", fg="red", bold=True, nl=True)
-    click.secho(f"PyPi: https://pypi.org/project/devd/", fg="yellow", bold=True, nl=True)
+    click.secho(
+        f"Github: https://github.com/pateash/devd", fg="red", bold=True, nl=True
+    )
+    click.secho(
+        f"PyPi: https://pypi.org/project/devd/", fg="yellow", bold=True, nl=True
+    )
+
 
 cli.add_command(hello)
 cli.add_command(info)
--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -1,6 +1,6 @@
 from click.testing import CliRunner
 
-from devd.cli import hello
+from hckr.cli import hello
 
 
 def test_hello_world():
