a1f9b02dd3936ec06da28109fb8f5ea553c6b7e7
Ashish Patel <ashishpatel0720@gmail.com>
2024-06-27 11:24:55 +0530

    automatic: release added

--- src/hckr/utils/CliUtils.py
+++ src/hckr/utils/CliUtils.py
@@ -5,7 +5,7 @@ import rich
 from packaging import version
 from rich.panel import Panel
 
-from .MessageUtils import colored, success
+from .MessageUtils import colored, success, warning
 from ..__about__ import __version__
 
 
@@ -24,17 +24,16 @@ def is_new_version_available(current_version, latest_version):
     current_ver = version.parse(current_version)
     latest_ver = version.parse(latest_version)
 
-    logging.info(f"current : {current_version}, latest: {latest_version}")
+    logging.debug(f"current : {current_version}, latest: {latest_version}")
 
     # Check if the latest version is greater than the current version
     return latest_ver > current_ver
 
 
-def check_update():
+def check_update(show_no_update=False):
     needs_update, latest_version = check_latest_version()
-    logging.info(needs_update, latest_version)
     if needs_update:
-        success(
+        warning(
             f"Info: Update available {colored(__version__, 'magenta')} ->  {colored(latest_version, 'green')}"
         )
         rich.print(
@@ -43,4 +42,8 @@ def check_update():
                 expand=False,
                 title="Run to upgrade",
             )
+        )
+    elif show_no_update:
+        success(
+            f"Success: You are using latest version {colored(__version__, 'magenta')}"
         )
