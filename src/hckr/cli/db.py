bbf4db8b0e6d0004a370cf86a5cc63c11bbd85d3
Ashish Patel <ashishpatel0720@gmail.com>
2024-08-24 20:24:05 +0530

    Refactor query command to return and display DataFrame
    Refactor the `query` command to execute SQL on Snowflake and return results as a DataFrame. Added imports for `config_path`, `print_df_as_table`, and `pandas`. Replaced row-by-row output with a formatted DataFrame table display.

--- src/hckr/cli/db.py
+++ src/hckr/cli/db.py
@@ -4,9 +4,11 @@ from sqlalchemy.exc import SQLAlchemyError
 from yaspin import yaspin  # type: ignore
 
 from hckr.cli.config import common_config_options
+from hckr.utils.ConfigUtils import config_path
+from hckr.utils.DataUtils import print_df_as_table
 from hckr.utils.DbUtils import get_db_url
 from hckr.utils.MessageUtils import PError
-
+import pandas as pd
 
 @click.group(
     help="Database commands",
@@ -21,15 +23,16 @@ def db():
 @click.argument("query")
 @click.pass_context
 def query(ctx, config, query):
-    """Execute a SQL query."""
+    """Execute a SQL query on Snowflake and return a DataFrame."""
     db_url = get_db_url(section=config)
+
     if db_url:
         engine = create_engine(db_url)
         try:
             with engine.connect() as connection:
-                result = connection.execute(text(query))
-                for row in result:
-                    click.echo(row)
+                # Execute the query and convert the result to a DataFrame
+                df = pd.read_sql_query(text(query), connection)
+                print_df_as_table(df)
         except SQLAlchemyError as e:
             PError(f"Error executing query: {e}")
     else:
